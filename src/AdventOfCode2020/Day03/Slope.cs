4a8450fb7b43454b9b5ebb4eac3f6886efd642c4
Imran Merchant <imerchant@outlook.com>
2020-12-05 00:44:03 -0600

    add Day 03 puzzle 1 solution

---
+++ src/AdventOfCode2020/Day03/Slope.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2020.Day03
+{
+    public class Slope
+    {
+        public const char Tree = '#';
+
+        public List<string> Grid { get; }
+
+        public (int Right, int Down) FirstSledAngle { get; } = (3, 1);
+
+        public int X { get; private set; } = 0;
+        public int Y { get; private set; } = 0;
+
+        public char CurrentLocation => Grid[Y][X % Grid[0].Length];
+
+        public Slope(string input)
+        {
+            Grid = input.SplitLines().ToList();
+        }
+
+        public int CountTreesOnDescent((int Right, int Down) angle)
+        {
+            var trees = 0;
+
+            do
+            {
+                if (CurrentLocation == Tree)
+                    trees++;
+
+                Y += angle.Down;
+                X += angle.Right;
+
+            } while (Y < Grid.Count);
+
+            return trees;
+        }
+    }
+}
---
+++ test/AdventOfCode2020.Tests/Day03Solutions.cs
@@ -0,0 +1,44 @@
+﻿using AdventOfCode2020.Day03;
+using AdventOfCode2020.Inputs;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode2020.Tests
+{
+    public class Day03Solutions : TestBase
+    {
+        public Day03Solutions(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void Puzzle1_CountTrees_UsingFirstAngle()
+        {
+            var slope = new Slope(Input.Day03);
+
+            slope.CountTreesOnDescent(slope.FirstSledAngle).Should().Be(211);
+        }
+
+        public const string PuzzleExample = 
+@"..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#";
+
+        [Fact]
+        public void PuzzleExample_CountsTreesCorrectly()
+        {
+            var slope = new Slope(PuzzleExample);
+
+            slope.CountTreesOnDescent(slope.FirstSledAngle).Should().Be(7);
+        }
+    }
+}
