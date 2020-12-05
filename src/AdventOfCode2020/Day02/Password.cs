5722b6df22fe70a2edf99b5dc9dc4d7fe2aa60f8
Imran Merchant <imerchant@outlook.com>
2020-12-05 00:24:33 -0600

    add Day 02 puzzle 2 solution

--- src/AdventOfCode2020/Day02/Password.cs
+++ src/AdventOfCode2020/Day02/Password.cs
@@ -8,23 +8,26 @@ namespace AdventOfCode2020.Day02
         public static readonly Regex Format = new Regex(@"(?'min'\d+)-(?'max'\d+) (?'char'\w): (?'content'\w+)");
 
         public string Content { get; }
-        public bool IsValid { get; }
+        public bool IsValidByCount { get; }
+        public bool IsValidByPosition { get; }
 
         public char PolicyChar { get; }
-        public int Minimum { get; }
-        public int Maximum { get; }
+        public int Param1 { get; } // minimum when by count
+        public int Param2 { get; } // maximum when by count
 
         public Password(string password)
         {
             var parsed = Format.Match(password);
 
-            Minimum = int.Parse(parsed.Groups["min"].Value);
-            Maximum = int.Parse(parsed.Groups["max"].Value);
+            Param1 = int.Parse(parsed.Groups["min"].Value);
+            Param2 = int.Parse(parsed.Groups["max"].Value);
             PolicyChar = parsed.Groups["char"].Value[0];
             Content = parsed.Groups["content"].Value;
 
             var count = Content.Count(x => x == PolicyChar);
-            IsValid = count >= Minimum && count <= Maximum;
+            IsValidByCount = count >= Param1 && count <= Param2;
+
+            IsValidByPosition = (Content[Param1 - 1] == PolicyChar) ^ (Content[Param2 - 1] == PolicyChar);
         }
     }
 }
--- test/AdventOfCode2020.Tests/Day02Solutions.cs
+++ test/AdventOfCode2020.Tests/Day02Solutions.cs
@@ -14,28 +14,48 @@ namespace AdventOfCode2020.Tests
         }
 
         [Fact]
-        public void Puzzle1_CountValidPasswords()
+        public void Puzzle1_CountValidPasswords_ByCount()
         {
             var input = Input.Day02Parse(Input.Day02);
 
             var passwords = new Passwords(input);
 
-            passwords.Count(x => x.IsValid).Should().Be(572);
+            passwords.Count(x => x.IsValidByCount).Should().Be(572);
         }
 
-        public const string Puzzle1Example =
+        [Fact]
+        public void Puzzle2_CountValidPasswords_ByPosition()
+        {
+            var input = Input.Day02Parse(Input.Day02);
+
+            var passwords = new Passwords(input);
+
+            passwords.Count(x => x.IsValidByPosition).Should().Be(306);
+        }
+
+        public const string PuzzleExample =
 @"1-3 a: abcde
 1-3 b: cdefg
 2-9 c: ccccccccc";
 
         [Fact]
-        public void Puzzle1Example_CountsValidPasswordsCorrectly()
+        public void Puzzle1_Example_CountsValidPasswords_ByCountCorrectly()
         {
-            var input = Input.Day02Parse(Puzzle1Example);
+            var input = Input.Day02Parse(PuzzleExample);
 
             var passwords = new Passwords(input);
 
-            passwords.Count(x => x.IsValid).Should().Be(2);
+            passwords.Count(x => x.IsValidByCount).Should().Be(2);
+        }
+
+        [Fact]
+        public void Puzzle2_ExampleCountsValidPasswords_ByPositionCorrectly()
+        {
+            var input = Input.Day02Parse(PuzzleExample);
+
+            var passwords = new Passwords(input);
+
+            passwords.Count(x => x.IsValidByPosition).Should().Be(1);
         }
     }
 }
