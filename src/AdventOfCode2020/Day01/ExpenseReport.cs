f948f0eef3966e2177f3053d762f4687505e566e
Imran Merchant <imerchant@outlook.com>
2020-12-04 23:54:06 -0600

    Add Day 01 puzzle 1 solution

---
+++ src/AdventOfCode2020/Day01/ExpenseReport.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode2020.Day01
+{
+    public class ExpenseReport
+    {
+        public List<int> Entries { get; }
+
+        public ExpenseReport(IEnumerable<int> entries)
+        {
+            Entries = new List<int>(entries);
+        }
+
+        public int Find2020PairAndMultiply()
+        {
+            for (var k = 0; k < Entries.Count; ++k)
+            {
+                for (var j = 0; j < Entries.Count; ++j)
+                {
+                    if (k == j) continue;
+
+                    if (Entries[k] + Entries[j] == 2020)
+                        return Entries[k] * Entries[j];
+                }
+            }
+
+            throw new Exception("Could not locate pair that adds to 2020");
+        }
+    }
+}
---
+++ test/AdventOfCode2020.Tests/Day01Solutions.cs
@@ -0,0 +1,27 @@
+﻿using AdventOfCode2020.Day01;
+using AdventOfCode2020.Inputs;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode2020.Tests
+{
+    public class Day01Solutions : TestBase
+    {
+        public Day01Solutions(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void Puzzle1_FindParThatAddsTo2020_AndMultiplyThem()
+        {
+            var entries = Input.Day01Parse(Input.Day01);
+
+            var expenseReport = new ExpenseReport(entries);
+
+            var result = expenseReport.Find2020PairAndMultiply();
+
+            result.Should().Be(1016131);
+        }
+    }
+}
