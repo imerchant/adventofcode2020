03a5396fe91dd222665385a06a5dfa9681563e2a
Imran Merchant <imerchant@outlook.com>
2020-12-08 00:03:11 -0600

    add Day 08 puzzle 1 solution

---
+++ src/AdventOfCode2020/Day08/GameConsole.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2020.Day08
+{
+    public class GameConsole
+    {
+        public static readonly Regex Format = new Regex(@"(?'op'nop|acc|jmp) (?'value'[+-]\d+)", RegexOptions.Compiled);
+
+        public List<Instruction> Instructions { get; }
+        public int Accumulator { get; private set; }
+
+        public GameConsole(string input)
+        {
+            Instructions = Format
+                .Matches(input)
+                .Select(Parse)
+                .ToList();
+
+            static Instruction Parse(Match match)
+            {
+                var op = match.Groups["op"].Value.ParseEnum<Op>(default);
+                var value = int.Parse(match.Groups["value"].Value);
+                return new Instruction(op, value);
+            }
+        }
+
+        public void FindSecondInstructionExecution()
+        {
+            for (var k = 0; k < Instructions.Count;)
+            {
+                var instruction = Instructions[k];
+                if (instruction.Visited)
+                    throw new Exception($"visited instruction[{k}] ({instruction}) with accumulator value {Accumulator}");
+
+                switch (instruction.Op)
+                {
+                    case Op.acc:
+                        Accumulator += instruction.Value;
+                        ++k;
+                        break;
+
+                    case Op.jmp:
+                        k += instruction.Value;
+                        break;
+
+                    case Op.nop:
+                    default:
+                        ++k;
+                        break;
+                }
+
+                instruction.Visited = true;
+            }
+        }
+    }
+
+    public class Instruction
+    {
+        public Op Op { get; }
+        public int Value { get; }
+        public bool Visited { get; set; }
+
+        public Instruction(Op op, int value)
+        {
+            Op = op;
+            Value = value;
+        }
+
+        public override string ToString() =>
+            new StringBuilder($"{Op:G}")
+                .Append(Value < 0 ? " " : " +")
+                .Append(Value)
+                .ToString();
+    }
+
+    public enum Op
+    {
+        nop,
+        acc,
+        jmp
+    }
+}
---
+++ test/AdventOfCode2020.Tests/Day08Solutions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using AdventOfCode2020.Day08;
+using AdventOfCode2020.Inputs;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode2020.Tests
+{
+    public class Day08Solutions : TestBase
+    {
+        public Day08Solutions(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void Puzzle1_FindSecondInstructionExecution()
+        {
+            var console = new GameConsole(Input.Day08);
+
+            Action run = () => console.FindSecondInstructionExecution();
+
+            run.Should().Throw<Exception>().WithMessage("visited instruction[350] (acc +3) with accumulator value 1475");
+        }
+
+        public const string Puzzle1Example =
+@"nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6";
+
+        [Fact]
+        public void Puzzle1Example_ConsoleCountsInstructions()
+        {
+            var console = new GameConsole(Puzzle1Example);
+
+            console.Instructions.Should().HaveCount(9);
+
+            Action run = () => console.FindSecondInstructionExecution();
+
+            run.Should().Throw<Exception>().WithMessage("visited instruction[1] (acc +1) with accumulator value 5");
+        }
+    }
+}
