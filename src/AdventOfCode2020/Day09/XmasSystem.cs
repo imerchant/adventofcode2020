20bafa0710f5676903c9c7d40551de8cae127b50
Imran Merchant <imerchant@outlook.com>
2020-12-09 00:08:55 -0600

    add Day 09 puzzles 1 and 2 solutions

---
+++ src/AdventOfCode2020/Day09/XmasSystem.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2020.Day09
+{
+    public class XmasSystem
+    {
+        public List<long> Numbers { get; }
+
+        public XmasSystem(string input)
+        {
+            Numbers = input.SplitLines().Select(long.Parse).ToList();
+        }
+
+        public long FindFirstInvalid(int preambleLength)
+        {
+            for (var k = preambleLength; k < Numbers.Count; ++k)
+            {
+                var start = k - preambleLength;
+                if (FindNumbersThatSumTo(Numbers[k], start, preambleLength) is false)
+                {
+                    return Numbers[k];
+                }
+            }
+
+            throw new Exception("Could not locate invalid number");
+
+            bool FindNumbersThatSumTo(long target, int start, int take)
+            {
+                var numbers = Numbers.Skip(start).Take(take).ToList();
+                for (var a = 0; a < numbers.Count; a++)
+                {
+                    for (var b = 0; b < numbers.Count; b++)
+                    {
+                        if (a == b) continue;
+                        if (numbers[a] + numbers[b] == target)
+                        {
+                            return true;
+                        }
+                    }
+                }
+
+                return false;
+            }
+        }
+
+        public List<long> FindSetThatAddsToInvalid(long target)
+        {
+            var set = new List<long>();
+
+            for (var k = 0; k < Numbers.Count; ++k)
+            {
+                set.Clear();
+
+                if (Numbers[k] >= target) continue;
+
+                set.Add(Numbers[k]);
+                for (var j = k+1; j < Numbers.Count; ++j)
+                {
+                    set.Add(Numbers[j]);
+                    var sum = set.Sum();
+
+                    if (sum == target)
+                    {
+                        return set;
+                    }
+
+                    if (sum > target)
+                        break;
+                }
+            }
+
+            throw new Exception($"Could not locate set that adds to target {target}");
+        }
+    }
+}
---
+++ test/AdventOfCode2020.Tests/Day09Solutions.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode2020.Day09;
+using AdventOfCode2020.Inputs;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode2020.Tests
+{
+    public class Day09Solutions : TestBase
+    {
+        public Day09Solutions(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void Puzzle1_FindFirstInvalid()
+        {
+            const int preambleLength = 25;
+            var system = new XmasSystem(Input.Day09);
+
+            system.FindFirstInvalid(preambleLength).Should().Be(138879426L);
+        }
+
+        [Fact]
+        public void Puzzle2_FundSumOfMinAndMaxOfSetThatAddsToInvalid()
+        {
+            const long firstInvalid = 138879426L;
+            var system = new XmasSystem(Input.Day09);
+
+            var set = system.FindSetThatAddsToInvalid(firstInvalid);
+
+            var (min, max) = (set.Min(), set.Max());
+
+            (min + max).Should().Be(23761694L);
+        }
+
+
+        public const string PuzzleExample =
+@"35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576";
+
+        [Fact]
+        public void PuzzleExample_FindFirstInvalid()
+        {
+            const int preambleLength = 5;
+            var system = new XmasSystem(PuzzleExample);
+
+            system.FindFirstInvalid(preambleLength).Should().Be(127);
+        }
+
+        [Fact]
+        public void PuzzleExample_FindSetThatAddsToInvalid()
+        {
+            var expectedSet = new List<long> { 15, 25, 47, 40 };
+            var system = new XmasSystem(PuzzleExample);
+
+            var actual = system.FindSetThatAddsToInvalid(127);
+
+            actual.Should().BeEquivalentTo(expectedSet);
+
+            var (min, max) = (actual.Min(), actual.Max());
+
+            (min + max).Should().Be(62);
+        }
+    }
+}
