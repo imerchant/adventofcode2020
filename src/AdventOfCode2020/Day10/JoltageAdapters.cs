09a3c9a2058b6144b0b92ceb28d446060d906f00
Imran Merchant <imerchant@outlook.com>
2020-12-10 22:54:53 -0600

    add Day10 puzzle 1 solution

---
+++ src/AdventOfCode2020/Day10/JoltageAdapters.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2020.Day10
+{
+    public class JoltageAdapters : IEnumerable<int>
+    {
+        public List<int> Adapters { get; }
+
+        public JoltageAdapters(string input)
+        {
+            Adapters = input.SplitLines().Select(int.Parse).ToList();
+
+            Adapters.Add(0);
+            Adapters = Adapters.OrderBy(x => x).ToList();
+            Adapters.Add(Adapters.Last() + 3);
+        }
+
+        public (int Of1, int Of3) GetDifferences()
+        {
+            var of1 = 0;
+            var of3 = 0;
+
+            for (var k = 0; k < Adapters.Count - 1; k++)
+            {
+                var first = Adapters[k];
+                var second = Adapters[k + 1];
+
+                var difference = second - first;
+
+                _ = difference switch
+                {
+                    1 => of1++,
+                    3 => of3++,
+                    _ => 0
+                };
+            }
+
+            return (of1, of3);
+        }
+
+        public IEnumerator<int> GetEnumerator() => Adapters.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ test/AdventOfCode2020.Tests/Day10Solutions.cs
@@ -0,0 +1,89 @@
+﻿using AdventOfCode2020.Day10;
+using AdventOfCode2020.Inputs;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode2020.Tests
+{
+    public class Day10Solutions : TestBase
+    {
+        public Day10Solutions(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void Puzzle1_FindProductOfDifferencesOf1AndOf3()
+        {
+            var adapters = new JoltageAdapters(Input.Day10);
+
+            var (of1, of3) = adapters.GetDifferences();
+
+            (of1 * of3).Should().Be(2432);
+        }
+
+        public const string Example1 =
+@"16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4";
+
+        [Fact]
+        public void Example1_Adapters_AreUpdatedAndSortedProperly()
+        {
+            var adapters = new JoltageAdapters(Example1);
+
+            adapters.Should().HaveCount(13).And.BeInAscendingOrder().And.StartWith(0).And.EndWith(22);
+
+            adapters.GetDifferences().Should().Be((7, 5));
+        }
+
+        public const string Example2 =
+@"28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3";
+
+        [Fact]
+        public void Example2_FindsDifferencesCorrectly()
+        {
+            var adapters = new JoltageAdapters(Example2);
+
+            adapters.GetDifferences().Should().Be((22, 10));
+        }
+    }
+}
