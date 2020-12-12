a01d78d2410fb17dae577d8f7034a7b02c2e03bc
Imran Merchant <imerchant@outlook.com>
2020-12-12 00:52:45 -0600

    add Day 12 puzzle 1 solution

---
+++ src/AdventOfCode2020/Day12/Ferry.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2020.Day12
+{
+    public class Ferry
+    {
+        public static readonly Regex Format = new Regex(@"(?'direction'[NESWRLF])(?'value'\d+)", RegexOptions.Compiled);
+
+        public List<(Direction Direction, int Value)> Instructions { get; }
+
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public Direction Facing { get; private set; }
+
+        public int ManhattanDistance => Math.Abs(X) + Math.Abs(Y);
+
+        public Ferry(string input)
+        {
+            Instructions = Format.Matches(input)
+                .Select(match => (
+                    match.Groups["direction"].Value.ParseEnum<Direction>(default),
+                    int.Parse(match.Groups["value"].Value)))
+                .ToList();
+        }
+
+        public void FollowInstructions()
+        {
+            (X, Y, Facing) = (0, 0, Direction.E);
+
+            foreach (var (direction, value) in Instructions)
+            {
+                switch (direction)
+                {
+                    case Direction.N:
+                    case Direction.E:
+                    case Direction.S:
+                    case Direction.W:
+                        Move(direction, value);
+                        break;
+
+                    case Direction.F:
+                        Move(Facing, value);
+                        break;
+
+                    case Direction.L:
+                    case Direction.R:
+                        Rotate(direction, value);
+                        break;
+                }
+            }
+
+            static Direction NextRight(Direction facing, int rotation)
+            {
+                do
+                {
+                    facing = facing switch
+                    {
+                        Direction.N => Direction.E,
+                        Direction.E => Direction.S,
+                        Direction.S => Direction.W,
+                        Direction.W => Direction.N,
+                        _ => throw new Exception("trying to find next facing with a direction that isn't valid")
+                    };
+
+                    rotation -= 90;
+                } while (rotation > 0);
+
+                return facing;
+            }
+
+            static Direction NextLeft(Direction facing, int rotation)
+            {
+                do
+                {
+                    facing = facing switch
+                    {
+                        Direction.N => Direction.W,
+                        Direction.W => Direction.S,
+                        Direction.S => Direction.E,
+                        Direction.E => Direction.N,
+                        _ => throw new Exception("trying to find next facing with a direction that isn't valid")
+                    };
+
+                    rotation -= 90;
+                } while (rotation > 0);
+
+                return facing;
+            }
+
+            void Rotate(Direction direction, int value)
+            {
+                Facing = direction switch
+                {
+                    Direction.L => NextLeft(Facing, value),
+                    Direction.R => NextRight(Facing, value),
+                    _ => throw new Exception("trying to rotate with a direction that isn't valid")
+                };
+            }
+
+            void Move(Direction direction, int value)
+            {
+                X = direction switch
+                {
+                    Direction.E => X + value,
+                    Direction.W => X - value,
+                    _ => X
+                };
+
+                Y = direction switch
+                {
+                    Direction.N => Y + value,
+                    Direction.S => Y - value,
+                    _ => Y
+                };
+            }
+        }
+    }
+
+    public enum Direction
+    {
+        N, E, S, W,
+        L, R,
+        F
+    }
+}
---
+++ test/AdventOfCode2020.Tests/Day12Solutions.cs
@@ -0,0 +1,57 @@
+﻿using AdventOfCode2020.Day12;
+using AdventOfCode2020.Inputs;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode2020.Tests
+{
+    public class Day12Solutions : TestBase
+    {
+        public Day12Solutions(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void Puzzle1_FollowInstructions_AndFindManhattanDistance()
+        {
+            var ferry = new Ferry(Input.Day12);
+
+            ferry.FollowInstructions();
+
+            ferry.ManhattanDistance.Should().Be(1645);
+        }
+
+        public const string Example =
+@"F10
+N3
+F7
+R90
+F11";
+
+        [Fact]
+        public void Example_ImportsInstructionsCorrectly()
+        {
+            var ferry = new Ferry(Example);
+
+            Assert.Collection(ferry.Instructions,
+                item => { item.Direction.Should().Be(Direction.F); item.Value.Should().Be(10); },
+                item => { item.Direction.Should().Be(Direction.N); item.Value.Should().Be(3); },
+                item => { item.Direction.Should().Be(Direction.F); item.Value.Should().Be(7); },
+                item => { item.Direction.Should().Be(Direction.R); item.Value.Should().Be(90); },
+                item => { item.Direction.Should().Be(Direction.F); item.Value.Should().Be(11); })
+                ;
+        }
+
+        [Fact]
+        public void Ferry_FollowInstructions_WithExample_EndsUpInCorrectPlace()
+        {
+            var ferry = new Ferry(Example);
+
+            ferry.FollowInstructions();
+
+            (ferry.X, ferry.Y).Should().Be((17, -8));
+            ferry.ManhattanDistance.Should().Be(25);
+        }
+    }
+}
