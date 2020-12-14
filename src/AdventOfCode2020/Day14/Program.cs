bc70ad97bafab82175a6a1c8a274a91a69469b7c
Imran Merchant <imerchant@outlook.com>
2020-12-13 23:42:59 -0600

    add Day 14 puzzle 1 solution

---
+++ src/AdventOfCode2020/Day14/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2020.Day14
+{
+    public class Program
+    {
+        public readonly Regex MaskFormat = new Regex(@"mask = (?'mask'[01X]+)", RegexOptions.Compiled);
+        public readonly Regex MemFormat = new Regex(@"mem\[(?'location'\d+)\] = (?'value'\d+)", RegexOptions.Compiled);
+
+        public IDictionary<int, long> Memory { get; }
+
+        public List<string> Instructions { get; }
+
+        public string Mask { get; private set; }
+
+        public Program(IEnumerable<string> instructions)
+        {
+            Memory = new DefaultDictionary<int, long>(_ => 0L);
+            Instructions = new List<string>(instructions);
+        }
+
+        public void Run()
+        {
+            foreach (var instruction in Instructions)
+            {
+                var maskMatch = MaskFormat.Match(instruction);
+                if (maskMatch.Success)
+                {
+                    Mask = maskMatch.Groups["mask"].Value;
+                    continue;
+                }
+
+                var memMatch = MemFormat.Match(instruction);
+                var location = int.Parse(memMatch.Groups["location"].Value);
+                var originalValue = long.Parse(memMatch.Groups["value"].Value);
+
+                var valAsString = Convert.ToString(originalValue, 2).PadLeft(Mask.Length, '0');
+                var valAsMask = new StringBuilder(valAsString);
+
+                for (var k = 1; k <= valAsMask.Length; ++k)
+                {
+                    var bit = valAsMask[^k];
+
+                    valAsMask[^k] = Mask[^k] switch
+                    {
+                        'X' => bit,
+                        char c => c
+                    };
+                }
+
+                var finalVal = valAsMask.ToString();
+                Memory[location] = Convert.ToInt64(finalVal, 2);
+            }
+        }
+    }
+}
---
+++ test/AdventOfCode2020.Tests/Day14Solutions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using AdventOfCode2020.Day14;
+using AdventOfCode2020.Inputs;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode2020.Tests
+{
+    public class Day14Solutions : TestBase
+    {
+        public Day14Solutions(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void Puzzle1_FindSumOfValues_AfterInstructions()
+        {
+            var program = new Program(Input.Day14Parse(Input.Day14));
+
+            program.Run();
+
+            program.Memory.Sum(x => x.Value).Should().Be(6631883285184L);
+        }
+
+        public const string Puzzle1Example =
+@"mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+mem[8] = 11
+mem[7] = 101
+mem[8] = 0";
+
+        [Fact]
+        public void Puzzle1Example_RunsInstructionsCorrectly()
+        {
+            var program = new Program(Input.Day14Parse(Puzzle1Example));
+
+            program.Run();
+
+            program.Memory.Sum(x => x.Value).Should().Be(165L);
+        }
+
+        [Theory]
+        [InlineData("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X", 11, 73)]
+        [InlineData("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X", 101, 101)]
+        [InlineData("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X", 0, 64)]
+        public void ApplicationOfMask_ResultsInCorrectValue(string mask, long originalValue, long expectedValue)
+        {
+            var asString = Convert.ToString(originalValue, 2).PadLeft(mask.Length, '0');
+            var valAsMask = new StringBuilder(asString);
+
+            for (var k = 1; k <= valAsMask.Length; ++k)
+            {
+                var bit = valAsMask[^k];
+
+                valAsMask[^k] = mask[^k] switch
+                {
+                    'X' => bit,
+                    char c => c
+                };
+            }
+
+            var finalVal = valAsMask.ToString();
+            var actualValueAfterMask = Convert.ToInt64(finalVal, 2);
+
+            actualValueAfterMask.Should().Be(expectedValue);
+        }
+    }
+}
