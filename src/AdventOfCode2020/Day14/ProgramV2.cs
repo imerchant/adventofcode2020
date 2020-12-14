18e79c31cf15f3c4bb3461c33118d29db7efcdc5
Imran Merchant <imerchant@outlook.com>
2020-12-14 00:19:08 -0600

    add Day 14 puzzle 2 solution

---
+++ src/AdventOfCode2020/Day14/ProgramV2.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2020.Day14
+{
+    public class ProgramV2
+    {
+        public readonly Regex MaskFormat = new Regex(@"mask = (?'mask'[01X]+)", RegexOptions.Compiled);
+        public readonly Regex MemFormat = new Regex(@"mem\[(?'location'\d+)\] = (?'value'\d+)", RegexOptions.Compiled);
+
+        public IDictionary<long, long> Memory { get; }
+
+        public List<string> Instructions { get; }
+
+        public string Mask { get; private set; }
+
+        public ProgramV2(IEnumerable<string> instructions)
+        {
+            Memory = new DefaultDictionary<long, long>(_ => 0L);
+            Instructions = new List<string>(instructions);
+        }
+
+        public void Run()
+        {
+            foreach (var instruction in Instructions)
+            {
+                var maskMatch = MaskFormat.Match(instruction);
+                if (maskMatch.Success)
+                {
+                    Mask = maskMatch.Groups["mask"].Value;
+                    continue;
+                }
+
+                var memMatch = MemFormat.Match(instruction);
+                var originalLocation = int.Parse(memMatch.Groups["location"].Value);
+                var value = long.Parse(memMatch.Groups["value"].Value);
+
+                var locationAsString = Convert.ToString(originalLocation, 2).PadLeft(Mask.Length, '0');
+                var locationAsMask = new StringBuilder(locationAsString);
+
+                for (var k = 1; k <= locationAsMask.Length; ++k)
+                {
+                    var bit = locationAsMask[^k];
+
+                    locationAsMask[^k] = Mask[^k] switch
+                    {
+                        'X' => 'X',
+                        '1' => '1',
+                        _ => bit
+                    };
+                }
+
+                var locationMasks = new HashSet<string>();
+                var stack = new Stack<string>();
+                stack.Push(locationAsMask.ToString());
+
+                do
+                {
+                    var mask = stack.Pop();
+                    var firstX = mask.IndexOf('X');
+                    if (firstX >= 0)
+                    {
+                        var builder = new StringBuilder(mask);
+                        builder[firstX] = '1';
+                        stack.Push(builder.ToString());
+
+                        builder[firstX] = '0';
+                        stack.Push(builder.ToString());
+                        continue;
+                    }
+
+                    locationMasks.Add(mask);
+                } while (stack.TryPeek(out _));
+
+                var locations = locationMasks.Select(x => Convert.ToInt64(x, 2));
+
+                foreach (var location in locations)
+                {
+                    Memory[location] = value;
+                }
+            }
+        }
+    }
+}
--- test/AdventOfCode2020.Tests/Day14Solutions.cs
+++ test/AdventOfCode2020.Tests/Day14Solutions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using AdventOfCode2020.Day14;
@@ -23,6 +24,16 @@ namespace AdventOfCode2020.Tests
             program.Run();
 
             program.Memory.Sum(x => x.Value).Should().Be(6631883285184L);
+        }
+
+        [Fact]
+        public void Puzzle2_FindSumOfValues_AfterInstructions_UsingV2Masking()
+        {
+            var program = new ProgramV2(Input.Day14Parse(Input.Day14));
+
+            program.Run();
+
+            program.Memory.Sum(x => x.Value).Should().Be(3161838538691L);
         }
 
         public const string Puzzle1Example =
@@ -65,6 +76,74 @@ mem[8] = 0";
             var actualValueAfterMask = Convert.ToInt64(finalVal, 2);
 
             actualValueAfterMask.Should().Be(expectedValue);
+        }
+
+        public const string Puzzle2Example =
+@"mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1";
+
+        [Fact]
+        public void Puzzle2Example_RunsInstructionsCorrectly()
+        {
+            var program = new ProgramV2(Input.Day14Parse(Puzzle2Example));
+
+            program.Run();
+
+            program.Memory.Sum(x => x.Value).Should().Be(208L);
+        }
+
+        public static IEnumerable<object[]> ApplicationOfV2MaskCases()
+        {
+            yield return new object[] { "000000000000000000000000000000X1001X", 42L, new List<long> { 26, 27, 58, 59 } };
+            yield return new object[] { "00000000000000000000000000000000X0XX", 26L, new List<long> { 16, 17, 18, 19, 24, 25, 26, 27 } };
+        }
+
+        [Theory]
+        [MemberData(nameof(ApplicationOfV2MaskCases))]
+        public void ApplicationsOfV2Mask_ResultsInCorrectLocations(string mask, long originalLocation, List<long> expectedLocations)
+        {
+            var locationAsString = Convert.ToString(originalLocation, 2).PadLeft(mask.Length, '0');
+            var locationAsMask = new StringBuilder(locationAsString);
+
+            for (var k = 1; k <= locationAsMask.Length; ++k)
+            {
+                var bit = locationAsMask[^k];
+
+                locationAsMask[^k] = mask[^k] switch
+                {
+                    'X' => 'X',
+                    '1' => '1',
+                    _ => bit
+                };
+            }
+
+            var locationMasks = new HashSet<string>();
+            var stack = new Stack<string>();
+            stack.Push(locationAsMask.ToString());
+
+            do
+            {
+                var locationMask = stack.Pop();
+                var firstX = locationMask.IndexOf('X');
+                if (firstX >= 0)
+                {
+                    var builder = new StringBuilder(locationMask);
+                    builder[firstX] = '1';
+                    stack.Push(builder.ToString());
+
+                    builder[firstX] = '0';
+                    stack.Push(builder.ToString());
+                    continue;
+                }
+
+                locationMasks.Add(locationMask);
+            } while (stack.TryPeek(out _));
+
+            var locations = locationMasks.Select(x => Convert.ToInt64(x, 2));
+
+            locations.Should().BeEquivalentTo(expectedLocations);
         }
     }
 }
