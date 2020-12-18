bca24d007d07f49143d114e2e7969d05d590f360
Imran Merchant <imerchant@outlook.com>
2020-12-18 16:13:43 -0600

    add Day 16 puzzle 1 solution

---
+++ src/AdventOfCode2020/Day16/Tickets.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2020.Day16
+{
+    public class Tickets
+    {
+        public IList<TicketField> Fields { get; }
+        public IList<IList<int>> Nearby { get; }
+
+        public Tickets(string fields, string nearbyTickets)
+        {
+            Fields = fields
+                .SplitLines()
+                .Select(x => new TicketField(x))
+                .ToList();
+
+            Nearby = nearbyTickets
+                .SplitLines()
+                .Select<string, IList<int>>(x => x.SplitOn(',').Select(int.Parse).ToList())
+                .ToList();
+        }
+
+        public int GetErrorRate()
+        {
+            var errorRate = 0;
+
+            foreach (var ticket in Nearby)
+            {
+                var invalidFields = ticket.Where(value => Fields.All(field => !field.IsValid(value)));
+                errorRate += invalidFields.Sum();
+            }
+
+            return errorRate;
+        }
+    }
+
+    public class TicketField
+    {
+        public static readonly Regex Format = new Regex(@"(?'name'.+): (?'low1'\d+)-(?'high1'\d+) or (?'low2'\d+)-(?'high2'\d+)", RegexOptions.Compiled);
+
+        public string Name { get; }
+
+        public int Low1 { get; }
+        public int High1 { get; }
+
+        public int Low2 { get; }
+        public int High2 { get; }
+
+        public TicketField(string content)
+        {
+            var match = Format.Match(content);
+
+            Name = match.Groups["name"].Value;
+            Low1 = int.Parse(match.Groups["low1"].Value);
+            High1 = int.Parse(match.Groups["high1"].Value);
+            Low2 = int.Parse(match.Groups["low2"].Value);
+            High2 = int.Parse(match.Groups["high2"].Value);
+        }
+
+        public bool IsValid(int value) => value >= Low1 && value <= High1 || value >= Low2 && value <= High2;
+    }
+}
---
+++ test/AdventOfCode2020.Tests/Day16Solutions.cs
@@ -0,0 +1,45 @@
+﻿using AdventOfCode2020.Day16;
+using AdventOfCode2020.Inputs;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode2020.Tests
+{
+    public class Day16Solutions : TestBase
+    {
+        public Day16Solutions(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public void Puzzle1_GetTicketErrorRate()
+        {
+            var tickets = new Tickets(Input.Day16.Fields, Input.Day16.NearbyTickets);
+
+            tickets.GetErrorRate().Should().Be(19093);
+        }
+
+        public const string ExampleFields =
+@"class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50";
+
+        public const string ExampleNearbyTickets =
+@"7,3,47
+40,4,50
+55,2,20
+38,6,12";
+
+        [Fact]
+        public void Example_CountsFields_AndErrorRateCorrectly()
+        {
+            var tickets = new Tickets(ExampleFields, ExampleNearbyTickets);
+
+            tickets.Fields.Should().HaveCount(3);
+            tickets.Nearby.Should().HaveCount(4);
+
+            tickets.GetErrorRate().Should().Be(71);
+        }
+    }
+}
