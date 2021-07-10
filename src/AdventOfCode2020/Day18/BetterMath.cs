ed70e0ebbda526fdaf01115f11de60397cfb0c0b
Imran Merchant <imerchant@outlook.com>
2021-07-09 21:17:08 -0500

    add Day 18 puzzle 2 solution

--- src/AdventOfCode2020/Day18/BetterMath.cs
+++ src/AdventOfCode2020/Day18/BetterMath.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -6,11 +7,13 @@ namespace AdventOfCode2020.Day18
 {
     public class BetterMath
     {
-        private static readonly Regex SimpleDigitsAndOp = new Regex(@"(?'operand1'\d+) (?'operator'[+*]) (?'operand2'\d+)", RegexOptions.Compiled);
+        private static readonly Regex DigitsAndEitherOperation = new Regex(@"(?'operand1'\d+) (?'operator'[+*]) (?'operand2'\d+)", RegexOptions.Compiled);
+        private static readonly Regex DigitsWithAddition = new Regex(@"(?'operand1'\d+) (\+) (?'operand2'\d+)", RegexOptions.Compiled);
+        private static readonly Regex DigitsWithMultiplication = new Regex(@"(?'operand1'\d+) (\*) (?'operand2'\d+)", RegexOptions.Compiled);
         private static readonly Regex DigitsInParens = new Regex(@"\((?'digits'\d+)\)", RegexOptions.Compiled);
         private static readonly Regex OnlyDigits = new Regex(@"^\d+$", RegexOptions.Compiled);
 
-        public long Calculate(string input, out List<string> iterations)
+        public long CalculateWithoutOperatorPrecedence(string input, out List<string> iterations)
         {
             var count = 0;
             iterations = new();
@@ -31,7 +34,7 @@ namespace AdventOfCode2020.Day18
                     continue;
                 }
 
-                var digitsAndOperationMatch = SimpleDigitsAndOp.Match(input);
+                var digitsAndOperationMatch = DigitsAndEitherOperation.Match(input);
                 if (digitsAndOperationMatch.Success)
                 {
                     var operand1 = long.Parse(digitsAndOperationMatch.Groups["operand1"].Value);
@@ -57,14 +60,97 @@ namespace AdventOfCode2020.Day18
             }
 
             return -1;
+        }
 
-            long Operate(long left, char op, long right) =>
-                op switch
+        public long CalculateWithNewPrecedence(string input, out List<string> iterations)
+        {
+            var count = 0;
+            iterations = new();
+            while (count++ < 200)
+            {
+                var builder = new StringBuilder(input);
+                iterations.Add(input);
+
+                var digitsInParensMatch = DigitsInParens.Match(input);
+                if (digitsInParensMatch.Success)
                 {
-                    '+' => left + right,
-                    '*' => left * right,
-                    _ => -1
-                };
+                    var num = long.Parse(digitsInParensMatch.Groups["digits"].Value);
+
+                    builder.Remove(digitsInParensMatch.Index, digitsInParensMatch.Length);
+                    builder.Insert(digitsInParensMatch.Index, $"{num}");
+
+                    input = builder.ToString();
+                    continue;
+                }
+
+                var digitsAdditionMatches = DigitsWithAddition.Matches(input);
+                if (digitsAdditionMatches.Any())
+                {
+                    var digitsAddition = digitsAdditionMatches
+                        .OrderByDescending(match => GetPriority(input, match))
+                        .First();
+
+                    var operand1 = long.Parse(digitsAddition.Groups["operand1"].Value);
+                    var operand2 = long.Parse(digitsAddition.Groups["operand2"].Value);
+
+                    var result = Operate(operand1, '+', operand2);
+
+                    builder.Remove(digitsAddition.Index, digitsAddition.Length);
+                    builder.Insert(digitsAddition.Index, $"{result}");
+
+                    input = builder.ToString();
+                    continue;
+                }
+
+                var digitsMultiplicationMatches = DigitsWithMultiplication.Matches(input);
+                if (digitsMultiplicationMatches.Any())
+                {
+                    var digitsMultiplication = digitsMultiplicationMatches
+                        .OrderByDescending(match => GetPriority(input, match))
+                        .First();
+
+                    var operand1 = long.Parse(digitsMultiplication.Groups["operand1"].Value);
+                    var operand2 = long.Parse(digitsMultiplication.Groups["operand2"].Value);
+
+                    var result = Operate(operand1, '*', operand2);
+
+                    builder.Remove(digitsMultiplication.Index, digitsMultiplication.Length);
+                    builder.Insert(digitsMultiplication.Index, $"{result}");
+
+                    input = builder.ToString();
+                    continue;
+                }
+
+                var onlyDigitsMatch = OnlyDigits.Match(input);
+                if (onlyDigitsMatch.Success)
+                {
+                    var num = long.Parse(input);
+
+                    return num;
+                }
+            }
+
+            return -1;
+        }
+
+        private static long Operate(long left, char op, long right) =>
+            op switch
+            {
+                '+' => left + right,
+                '*' => left * right,
+                _ => -1
+            };
+
+        private static int GetPriority(string input, Match match)
+        {
+            var priority = 0;
+            for (var k = 0; k < match.Index; ++k)
+            {
+                if (input[k] == '(') priority++;
+                if (input[k] == ')') priority--;
+            }
+
+            return priority;
         }
     }
 }
--- test/AdventOfCode2020.Tests/Day18Solutions.cs
+++ test/AdventOfCode2020.Tests/Day18Solutions.cs
@@ -18,12 +18,26 @@ namespace AdventOfCode2020.Tests
         }
 
         [Fact]
+        public void Puzzle2_SumAllInputCalculations_WithSwappedPrecedence()
+        {
+            var sum = new BigInteger(0);
+            foreach (var line in Input.Day18Parse(Input.Day18))
+            {
+                var result = _betterMath.CalculateWithNewPrecedence(line, out var iterations);
+                result.Should().NotBe(-1);
+                sum += result;
+            }
+
+            sum.Should().Be(224973686321527L);
+        }
+
+        [Fact]
         public void Puzzle1_SumAllInputCalculations()
         {
             var sum = new BigInteger(0);
             foreach (var line in Input.Day18Parse(Input.Day18))
             {
-                var result = _betterMath.Calculate(line, out var iterations);
+                var result = _betterMath.CalculateWithoutOperatorPrecedence(line, out var iterations);
                 result.Should().NotBe(-1);
                 sum += result;
             }
@@ -43,7 +57,7 @@ namespace AdventOfCode2020.Tests
         [InlineData("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 13632)]
         public void Puzzle1Examples_CalculateCorrectly(string input, int expectedValue)
         {
-            var result = _betterMath.Calculate(input, out var iterations);
+            var result = _betterMath.CalculateWithoutOperatorPrecedence(input, out var iterations);
 
             foreach (var iteration in iterations)
             {
@@ -63,6 +77,25 @@ namespace AdventOfCode2020.Tests
             builder.Insert(0, "3");
 
             builder.ToString().Should().Be("3 + 3");
+        }
+
+        [Theory]
+        [InlineData("1 + 2 * 3 + 4 * 5 + 6", 231)]
+        [InlineData("1 + (2 * 3) + (4 * (5 + 6))", 51)]
+        [InlineData("2 * 3 + (4 * 5)", 46)]
+        [InlineData("5 + (8 * 3 + 9 + 3 * 4 * 3)", 1445)]
+        [InlineData("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 669060)]
+        [InlineData("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 23340)]
+        public void Puzzle2Example_CalculatesCorrectly(string input, int expectedValue)
+        {
+            var result = _betterMath.CalculateWithNewPrecedence(input, out var iterations);
+
+            foreach (var iteration in iterations)
+            {
+                Output.WriteLine(iteration);
+            }
+
+            result.Should().Be(expectedValue);
         }
     }
 }
